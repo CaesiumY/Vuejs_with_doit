250061751726e46dd6b95b694a338e8d8c8f5e93
caesiumy <mn0658@naver.com>
2019-06-03 22:33:02 +0900

    feat: Update 06-4 todolist vue template

--- 06-2/src/components/TodoList.vue
+++ 06-2/src/components/TodoList.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        list
-    </div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="index" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{todoItem}}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+            <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-    export default {
-        
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
     }
+  }
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
