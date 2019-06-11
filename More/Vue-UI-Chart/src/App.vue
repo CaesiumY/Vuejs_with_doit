e6e3554bce22f117d9b97e517e0ace68f9b367f1
caesiumy <mn0658@naver.com>
2019-06-11 21:18:10 +0900

    feat: Add Vue-UI

---
+++ More/Vue-UI-Chart/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+    <ChartVue></ChartVue>
+  </div>
+</template>
+
+<script>
+import ChartVue from './Chart.vue';
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      ChartVue
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
